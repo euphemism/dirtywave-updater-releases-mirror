4a31451e5191f77cfe11d4d50d5c003e90ff66be
Nicholas Sidwell <nicholas@sidwell.dev>
2025-10-08 14:53:54 -0700

    tweak release flow

--- devenv.nix
+++ devenv.nix
@@ -3,7 +3,7 @@
 let
   # Source of truth.
   # Automatically propagates to src-tauri/Cargo.toml and src-quasar/package.json
-  application-version = "0.2.6";
+  application-version = "0.2.7";
 
   pkgs-unstable = import inputs.nixpkgs-unstable {
     overlays = [ inputs.rust-overlay.overlays.default ];
